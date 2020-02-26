c15a2561bed99e131f46fd333b670be3dc9ac17f
James McC <mariobud@gmail.com>
2020-02-26 17:02:17 -0600

    first commit

---
+++ PracticeScript1.ps1
@@ -0,0 +1,20 @@
+﻿#PowerShell Exercises 1
+
+#Folder and file manipulation
+
+New-Item -Path 'C:\Users\James\Desktop\TestFolder' -ItemType Directory
+New-Item -Path 'C:\Users\James\Desktop\TestFolder\Test File.txt' -ItemType File
+Copy-Item 'C:\Users\James\Desktop\TestFolder' -Destination 'C:\Users\James\Desktop\TestFolder\TestFolderCopy'
+Remove-Item 'C:\Users\James\Desktop\TestFolder\TestFolderCopy'
+Rename-Item -Path 'C:\Users\James\Desktop\TestFolder\Test File.txt' -NewName 'TestFileRenamed.txt'
+Get-Content 'C:\Users\James\Desktop\TestFolder\TestFileRenamed.txt'
+(Get-Content C:\Users\James\Desktop\TestFolder\TestFileRenamed.txt).Length
+Test-Path 'C:\Users\James\Desktop\TestFolder'
+Remove-item 'C:\Users\James\Desktop\TestFolder'
+
+# Time Exercies
+
+Get-Date
+Set-Date -Date (Get-Date).AddDays(-1);
+Get-Date -DisplayHint Date
+Get-Date -DisplayHint Time
